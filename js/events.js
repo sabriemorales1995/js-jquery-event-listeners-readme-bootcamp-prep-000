5f942eb536fc73d3a0eebaf10657415e5ff25921
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-14 15:33:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+  $('p').on("click",function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $(document).on("load",function(){
+    //adds the class "tasty" to the image to add a red frame to the image -- .addClass()
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('form input').on("keydown", function(key){
+    if(key.which===71||key.which===103)  {
+      alert('Youg pressed the letter G');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+frameIt();
 });
